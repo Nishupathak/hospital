533ded0ed570d8b5968bac677b5719c117cb64ed
Nishu Pathak <nishupathak23@gmail.com>
2023-09-18 16:06:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+let openMap = () => {
+    window.location.assign("https://www.bing.com/maps?&mepi=106~Local~Unknown~Map_Image&ty=18&q=ESI%20Hospital&ss=ypid.YN4070x4126317716524615233&segment=Local&ppois=28.528791427612305_77.26737976074219_ESI%20Hospital_YN4070x4126317716524615233~&cp=28.528791~77.26738&lvl=16&v=2&sV=1&FORM=SNAPST")
+}
